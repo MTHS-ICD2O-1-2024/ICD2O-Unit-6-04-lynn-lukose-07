9fd8c7b4737279b97f46d0a027b08abe96accf98
Lynn Lukose <lynn.lukose@stu.ocsb.ca>
2025-05-27 22:26:53 +0000

    heehee

--- js/script.js
+++ js/script.js
@@ -1,5 +1,26 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Lynn Lukose
+// Created on: May 2025
 // This file contains the JS functions for index.html
+
+"use strict"
+
+window.onload = function () {
+
+  const params = new URLSearchParams(document.location.search)
+
+  // input
+  const radius = params.get("radius")
+
+  // process
+  const volume = 4 / 3 * Math.PI * radius ** 3
+  const dimensions =
+    "<ul>\n<li>radius = " +
+    radius +
+    "</li>\n</ul>"
+
+  // output
+  document.getElementById("dimensions").innerHTML = dimensions
+  document.getElementById("volume").innerHTML = "volume is: " + volume.toFixed(3) + " mm³"
+}
